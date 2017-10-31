9f292d887ec70692643d58a31c7d4316a76e833d
David Demaree <david@demaree.me>
2017-10-31 10:53:25 -0400

    Modify PL-HL-WP theme to not redirect on /

--- wordpress/wp-content/themes/postlight-headless-wp/index.php
+++ wordpress/wp-content/themes/postlight-headless-wp/index.php
@@ -4,6 +4,11 @@ if ( is_single() ) {
 	header( 'Location: /wp-json/wp/v2/posts/' . get_post()->ID );
 } elseif ( is_page() ) {
 	header( 'Location: /wp-json/wp/v2/pages/' . get_queried_object()->ID );
-} else {
-	header( 'Location: /wp-json/' );
-}
+} ?>
+<!DOCTYPE html>
+<html>
+<head><title><?php echo $_SERVER['HTTP_HOST'] ?></title></head>
+<body>
+<!-- Hi, this WordPress site is headless -->
+</body>
+</html>
